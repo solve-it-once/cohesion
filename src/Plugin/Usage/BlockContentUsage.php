710ca2b5d2911d429bf175b6280a9e9c1eec3be7
Jenkins <florent.metz@acquia.com>
2020-08-07 15:01:55 +0100

    fix cypress tests

---
+++ src/Plugin/Usage/BlockContentUsage.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Drupal\cohesion\Plugin\Usage;
+
+use Drupal\cohesion\UsagePluginBase;
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\field\Entity\FieldConfig;
+
+/**
+ * Class BlockContentUsage.
+ *
+ * @package Drupal\cohesion\Plugin\Usage
+ *
+ * @Usage(
+ *   id = "block_content_usage",
+ *   name = @Translation("Block content usage"),
+ *   entity_type = "block_content",
+ *   scannable = TRUE,
+ *   scan_same_type = FALSE,
+ *   group_key = FALSE,
+ *   group_key_entity_type = FALSE,
+ *   exclude_from_package_requirements = TRUE,
+ *   exportable = FALSE,
+ *   config_type = "core",
+ *   scan_groups = {"site_studio", "core"}
+ * )
+ */
+class BlockContentUsage extends FieldableContentEntityUsageBase {
+
+}
